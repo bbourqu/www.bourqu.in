2b144bb1f2f659b17afd64903f78f3891bcc25a5
bbourqu <brett@bourqu.in>
2016-01-10 20:47:14 -0600

    Got dust motes almost working in build system

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 /*global particlesJS*/
 'use strict';
-particlesJS.load('dust-motes', 'assets/particles.json', function(){
+particlesJS.load('dust-motes', 'scripts/dust.json', function(){
     console.log('making dust motes...dang its dirty in here');
 });
+
